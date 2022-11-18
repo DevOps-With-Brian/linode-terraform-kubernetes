7d7931f14bd19ff44e23f4e94c7291997b5d3b10
Brian Hopkins <brianhh1230@gmail.com>
2022-11-18 06:52:09 -0500

    fix(cleanup): updating the code and some workflows

--- dns/main.tf
+++ dns/main.tf
@@ -18,21 +18,12 @@ resource "linode_domain" "master_domain" {
     tags = var.tags
 }
 
-
 resource "linode_domain_record" "rasa_dns_record" {
     domain_id = linode_domain.master_domain.id
     name = var.rasa_dns
     record_type = "A"
     target = var.nodebalancer_ip
 }
-
-# resource "linode_domain_record" "ns_records" {
-#     domain_id = linode_domain.master_domain.id
-#     count = length(var.linode_nameservers)
-#     name = var.linode_nameservers[count.index]
-#     record_type = "NS"
-#     target = var.domain_name
-# }
 
 resource "linode_domain_record" "github_pages" {
     domain_id = linode_domain.master_domain.id
